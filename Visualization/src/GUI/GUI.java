f5d18e014fd88cc6d2ae345703aac83a7a05a130
bretthartel <bretthartel@hotmail.com>
2015-11-14 16:53:12 -0700

    Add a faux GUI to our project for testing

---
+++ Visualization/src/GUI/GUI.java
@@ -0,0 +1,14 @@
+package GUI;
+
+/**
+ * Add a faux GUI to test the implementation of our visualization.
+ */
+public class GUI {
+
+  GUI(){}
+
+  public static void main(String[] args) {
+    // write your code here
+    new GUI();
+  }
+}
