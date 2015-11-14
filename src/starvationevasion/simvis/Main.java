658a80d324c013edb1e25c20d11c90125fe42985
bretthartel <bretthartel@hotmail.com>
2015-11-14 14:21:42 -0700

    added a working space

---
+++ src/starvationevasion/simvis/Main.java
@@ -0,0 +1,8 @@
+package starvationevasion.simvis;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/starvationevasion/simvis/brett/BrettMain.java
@@ -0,0 +1,11 @@
+package starvationevasion.simvis.brett;
+
+/**
+ * Created by Brett on 11/14/2015.
+ */
+public class BrettMain {
+
+  public static void main(String[] args) {
+    // write your code here
+  }
+}
