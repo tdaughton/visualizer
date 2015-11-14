a90f2712a32660265eae1cd0d39043c679c39c98
bretthartel <bretthartel@hotmail.com>
2015-11-14 15:42:34 -0700

    adding structure

---
+++ Visualization/src/starvationevasion/simvis/main.java
@@ -0,0 +1,11 @@
+package starvationevasion.simvis;
+
+/**
+ * Created by Brett on 11/14/2015.
+ */
+public class main {
+  public static void main(String[] args) {
+    // write your code here
+    System.out.println("Testing Main (simvis)");
+  }
+}
---
+++ Visualization/src/starvationevasion/tess/main.java
@@ -0,0 +1,11 @@
+package starvationevasion.tess;
+
+/**
+ * Created by Brett on 11/14/2015.
+ */
+public class main {
+  public static void main(String[] args) {
+    // write your code here
+    System.out.println("Testing Main (tess)");
+  }
+}
