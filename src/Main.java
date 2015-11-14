2fec93eec18f6b1368fbf2d9fa5f679c3a5abc0c
bretthartel <bretthartel@hotmail.com>
2015-11-14 15:00:14 -0700

    .idea

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-package starvationevasion.simvis.brett;
-
 /**
  * Created by Brett on 11/14/2015.
  */
-public class BrettMain {
-
+public class Main {
   public static void main(String[] args) {
     // write your code here
+    System.out.println("Test");
   }
 }
